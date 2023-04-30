d99bc17df1f6f1b8ca5e19273f0839b42d795f7e
Anton-Kandaurov <kand.anton2008@gmail.com>
2023-04-30 21:00:44 +0300

    start work

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './user.json';
+import { UserInfo } from './components/user-info/userInfo';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <UserInfo
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
   </React.StrictMode>
 );
